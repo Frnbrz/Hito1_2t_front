c466b85d7973f282b9583d099e62b6e202dbcf8c
frnbrz <frnbrz@gmail.com>
2023-12-20 17:46:50 +0100

    first commit

---
+++ app/client/src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <button
+      onClick={async () => {
+        const response = await fetch('/api/v1')
+        const data = await response.text()
+        console.log(data)
+      }}
+    >
+      Click me
+    </button>
+  )
+}
+export default App
