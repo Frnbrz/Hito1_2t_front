eb3db2dd161905586cde993135335105d88229a8
frnbrz <frnbrz@gmail.com>
2023-12-21 22:00:26 +0100

    Add global interceptors and pipes, and log thread information

--- app/api/src/main.ts
+++ app/api/src/main.ts
@@ -1,9 +1,22 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import * as os from 'os';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api/v1');
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+  console.log('Number of threads available: ', os.cpus().length);
+  console.log('Number of threads being used: ', process.env.UV_THREADPOOL_SIZE);
+
   await app.listen(3000);
 }
+
 bootstrap();
