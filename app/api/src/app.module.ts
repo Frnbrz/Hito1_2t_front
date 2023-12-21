79c5737b4837ad3e85611f2c806079defd37d541
frnbrz <frnbrz@gmail.com>
2023-12-21 22:00:23 +0100

    Add TypeOrmModule and LoggingInterceptor to AppModule

--- app/api/src/app.module.ts
+++ app/api/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { LoggingInterceptor } from './commons/loggin.interceptor';
 
 @Module({
   imports: [
@@ -11,8 +14,25 @@ import { UsersModule } from './users/users.module';
       rootPath: join(__dirname, '../../', 'client/dist'),
     }),
     UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5440,
+      database: 'hito_db',
+      username: 'root',
+      password: 'root',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
+  ],
 })
 export class AppModule {}
