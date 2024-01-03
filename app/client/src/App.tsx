310968af0b7939f3931019ba503717c68f043726
frnbrz <frnbrz@gmail.com>
2024-01-04 00:07:01 +0100

    Add .env and zitadel-compose.yml to .gitignore, update API endpoint in App.tsx

--- app/client/src/App.tsx
+++ app/client/src/App.tsx
@@ -2,9 +2,10 @@ function App() {
   return (
     <button
       onClick={async () => {
-        const response = await fetch('/api/v1/')
-        const data = await response.text()
-        console.log(data)
+        const response = await fetch('https://docs.google.com/spreadsheets/d/e/2PACX-1vQXXR6qX9UlHQz27QV0XaHCHfklBScja4bXMOV1sOlGVw7HIttXMZWX4VCkykphrAD4SiHZ3Efi4Mne/pub?output=tsv')
+
+        const text = await response.text()
+        console.log(text.split('\n'))
       }}
     >
       Click me
