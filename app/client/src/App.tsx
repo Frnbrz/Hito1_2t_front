beb024e81c1a2848e5eb2485172b5a737585cdee
frnbrz <frnbrz@gmail.com>
2024-01-06 20:16:49 +0100

    Refactor App.tsx to use Button component

--- app/client/src/App.tsx
+++ app/client/src/App.tsx
@@ -1,44 +1,8 @@
-import '@/App.css'
-import { ButtonAppBar } from '@/components/ButtonAppBar'
-import { AuthGuard, RoleGuard } from '@/guards'
-import { PrivateRoutes, PublicRoutes, Roles } from '@/models'
-import { Dashboard } from '@/pages/Private'
-import { ErrorBoundary, RoutesWithNotFound } from '@/utilities'
-import { SnackbarProvider } from 'notistack'
-import { Suspense, lazy } from 'react'
-import { BrowserRouter, Navigate, Route } from 'react-router-dom'
-import { Register } from './pages/Register'
-import { SnackbarUtilitiesConfigurator } from './utilities/snackbarManager'
-
-const Login = lazy(() => import('./pages/Login/Login'))
-const Private = lazy(() => import('./pages/Private/Private'))
+import { Button } from "@/components/ui/button"
 
 function App() {
   return (
-    <div className="App">
-      <SnackbarProvider>
-        <SnackbarUtilitiesConfigurator />
-        <Suspense fallback={<>Cargando</>}>
-            <BrowserRouter>
-              <ButtonAppBar />
-              <ErrorBoundary fallbackComponent={<h2>Hubo un error</h2>}>
-                <RoutesWithNotFound>
-                  <Route path="/" element={<Navigate to={PrivateRoutes.PRIVATE} />} />
-                  <Route path={PublicRoutes.LOGIN} element={<Login />} />
-                  <Route path={PublicRoutes.REGISTER} element={<Register />} />
-                  <Route element={<AuthGuard privateValidation />}>
-                    <Route path={`${PrivateRoutes.PRIVATE}/*`} element={<Private />} />
-                  </Route>
-                  <Route element={<RoleGuard rol={Roles.ADMIN} />}>
-                    <Route path={PrivateRoutes.DASHBOARD} element={<Dashboard />} />
-                  </Route>
-                </RoutesWithNotFound>
-              </ErrorBoundary>
-            </BrowserRouter>
-        </Suspense>
-        </SnackbarProvider>
-    </div>
+    <Button>Hola</Button>
   )
 }
-
 export default App
