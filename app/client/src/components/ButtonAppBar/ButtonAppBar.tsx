efc4382ca2660741428f0f0aef3b6008f6ada1d7
frnbrz <frnbrz@gmail.com>
2024-01-27 21:51:27 +0100

    Add private routes and logos to ButtonAppBar component

--- app/client/src/components/ButtonAppBar/ButtonAppBar.tsx
+++ app/client/src/components/ButtonAppBar/ButtonAppBar.tsx
@@ -1,9 +1,10 @@
-import { PublicRoutes } from '@/models'
+import { PrivateRoutes, PublicRoutes } from '@/models'
 import { UserKey, resetUser } from '@/redux/states/user'
 import { AppStore } from '@/redux/store'
 import { clearLocalStorage } from '@/utilities'
 import { useDispatch, useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import Logos from '../atoms/logos'
 
 export default function ButtonAppBar() {
   const navigate = useNavigate()
@@ -17,49 +18,28 @@ export default function ButtonAppBar() {
   }
   return (
     <div className="flex justify-between items-center p-4">
-
+      <div className="flex items-center">
+        <Logos.Home className="w-12 h-12 mr-4" />
+      </div>
       {userState.email ? (
         <>
-          <nav className=" border-gray-200 m-auto">
-            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-7">
-
-              <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-                <span className="sr-only">Open main menu</span>
-                <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                  <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
-                </svg>
-              </button>
-              <div className="hidden w-full md:block md:w-auto" id="navbar-default" >
-                <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                  <li>
-                    <a href="#" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
-                  </li>
-                  <li>
-                    <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
-                  </li>
-                  <li>
-                    <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
-                  </li>
-                  <li>
-                    <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a>
-                  </li>
-                  <li>
-                    <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
-                  </li>
-                </ul>
-
-              </div>
-              <button
-                className="text-white bg-red-500 hover:bg-red-600 py-2 px-4 rounded m-2"
-                onClick={logOut}
-              >
-                Logout
-              </button>
-            </div>
-
+          <nav>
+            <Link to={`/private/${PrivateRoutes.HOME}`} className="text-white mr-4">
+              {PrivateRoutes.HOME}
+            </Link>
+            <Link to={`/private/${PrivateRoutes.PROFILE}`} className="text-white mr-4">
+              {PrivateRoutes.PROFILE}
+            </Link>
+            <Link to={`/private/${PrivateRoutes.POST}`} className="text-white mr-4">
+              {PrivateRoutes.POST}
+            </Link>
           </nav>
-
-
+          <button
+            className="text-white bg-red-500 hover:bg-red-600 py-2 px-4 rounded"
+            onClick={logOut}
+          >
+            Logout
+          </button>
         </>
       ) : null}
     </div>
