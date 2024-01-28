f4b3f3d870ea4267941f087ed11b19155cde8ebf
frnbrz <frnbrz@gmail.com>
2024-01-29 00:18:29 +0100

    Refactor useDecoded hook to include DecodedToken type

--- app/client/src/hooks/useDecoded.ts
+++ app/client/src/hooks/useDecoded.ts
@@ -1,15 +1,14 @@
+import { DecodedToken } from "@/models"
 import { AppStore } from "@/redux/store"
 import { jwtDecode } from "jwt-decode"
 import { useSelector } from "react-redux"
 
 const useDecoded = () => {
 
-
-
   const userState = useSelector((store: AppStore) => store.user)
   const token = userState.token
   if (!token) return { role: '', email: '' }
-  const { role, email } = jwtDecode(token)
+  const { role, email }: DecodedToken = jwtDecode(token)
 
   return {
     role,
