a5b978ec1731223c1dc532b4903d40eb3044a58e
frnbrz <frnbrz@gmail.com>
2024-01-29 00:18:46 +0100

    Add CommentDelete and CommentUpdate components

---
+++ app/client/src/pages/Private/Post/CommentDelete.tsx
@@ -0,0 +1,59 @@
+import { AltButton, DefaultButton } from "@/components/atoms/button"
+import { PostModalItemProps } from "@/models"
+import { fetchBlogById } from "@/redux/states/blog"
+import { getPostById } from "@/services"
+import { deleteComment } from "@/services/comment.service"
+import { useDispatch } from "react-redux"
+import { useParams } from "react-router-dom"
+
+
+function CommentDelete({ onClose, id }: PostModalItemProps) {
+  const dispatch = useDispatch()
+  const { id: idBlog } = useParams()
+
+  function handleClose(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    event.preventDefault()
+    onClose()
+  }
+
+  async function handleDelete(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    console.log(id)
+
+    if (!id) return
+
+    await deleteComment(id)
+
+
+
+    await getPostById(Number(idBlog)).then((res) => {
+      dispatch(fetchBlogById(res.data))
+      onClose()
+    }
+    )
+
+  }
+
+
+  return (
+    <form className="max-w-sm mx-auto" onSubmit={(event) => handleDelete(event)}>
+      <div className="mb-5">
+
+        <p>
+          ¿Estás seguro que deseas eliminar este comentario?
+        </p>
+
+      </div>
+      <div className="flex justify-between">
+
+        <DefaultButton
+          onClick={handleClose}
+        >Cancelar</DefaultButton>
+
+        <AltButton>Borrar</AltButton>
+      </div>
+    </form>
+
+  )
+}
+export default CommentDelete
---
+++ app/client/src/services/comment.service.ts
@@ -0,0 +1,23 @@
+import { Comment } from "@/models"
+import axios from "axios"
+
+const URLAUTH = '/api/v1/comment'
+
+
+export const postComment = ({ text, blogId }: Comment) => {
+  return axios.post(URLAUTH, {
+    blogId,
+    text
+  })
+}
+
+export const updateComment = ({ id, text }: Comment) => {
+  return axios.put(URLAUTH + '/' + id, {
+    text
+  })
+}
+
+export const deleteComment = (id: number) => {
+  return axios.delete(URLAUTH + '/' + id)
+}
+
