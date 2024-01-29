1afac4ca1fe323d7c5523ee40b0fb8bf328828cc
Mona Lisa <johndoe@example.com>
2024-01-29 14:47:42 +0100

    Update ErrorPage component to use black text color

--- app/client/src/components/ErrorPage/ErrorPage.tsx
+++ app/client/src/components/ErrorPage/ErrorPage.tsx
@@ -3,7 +3,7 @@ function ErrorPage() {
   return (
     <div className="flex items-center justify-center h-screen ">
       <div className="bg-white p-8 rounded shadow">
-        <h1 className="text-2xl font-bold mb-4">Error</h1>
+        <h1 className="text-2xl font-bold mb-4 text-black">Error</h1>
         <p className="text-gray-600">Oops! Algo ha ido mal.</p>
       </div>
     </div>
