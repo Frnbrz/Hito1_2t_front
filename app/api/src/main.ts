82ef7f8f9e039c596277737a65e9d03768f9f7aa
Mona Lisa <johndoe@example.com>
2024-01-30 10:44:54 +0100

    Add Swagger documentation to API

--- app/api/src/main.ts
+++ app/api/src/main.ts
@@ -1,6 +1,8 @@
 import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger"
 import { AppModule } from './app.module'
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
@@ -13,6 +15,15 @@ async function bootstrap() {
     forbidNonWhitelisted: true,
     transform: true,
   }))
+
+  const config = new DocumentBuilder()
+    .setTitle("Blog example")
+    .setDescription("The blog API description")
+    .setVersion("1.0")
+    .addBearerAuth()
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup("docs", app, document)
 
   await app.listen(5432)
 }
