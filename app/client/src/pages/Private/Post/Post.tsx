69bf8073b13aa8766b0c365d1a38bccdca12e6a2
Mona Lisa <johndoe@example.com>
2024-01-31 10:09:24 +0100

    Update text color in Post component

--- app/client/src/pages/Private/Post/Post.tsx
+++ app/client/src/pages/Private/Post/Post.tsx
@@ -49,7 +49,7 @@ function Post() {
             key={blog.id}
           >
             <header className="mt-2 mb-4 flex justify-between " key={blog.id}>
-              <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-100">
                 <Link to={`/post/${blog.id}`}>{blog.name}</Link>
               </h2>
               {role === Roles.ADMIN ?
