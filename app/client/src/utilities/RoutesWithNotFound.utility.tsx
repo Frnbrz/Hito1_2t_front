3504e053b7de95b41afbef6848d65293ce90e8ce
frnbrz <frnbrz@gmail.com>
2024-02-02 00:56:33 +0100

    Refactor RoutesWithNotFound utility component

--- app/client/src/utilities/RoutesWithNotFound.utility.tsx
+++ app/client/src/utilities/RoutesWithNotFound.utility.tsx
@@ -7,20 +7,15 @@ function RoutesWithNotFound({ children }: Props) {
   return (
     <Routes>
       {children}
-      <Route
-        path="*"
-        element={
-          <section>
-
-            <div className="flex flex-col items-center justify-center h-screen">
-              <h2 className="text-2xl font-bold mb-4 text-white">Ruta no encontrada</h2>
-
-              <Link to={'/'} className="text-blue-500 hover:underline">Volver al Inicio</Link>
-            </div>
-          </section>
-        }
-      />
-    </Routes>
+      <Route path="*" element={
+        <section>
+          <div className="flex flex-col items-center justify-center h-screen">
+            <h2 className="text-2xl font-bold mb-4 text-white">Ruta no encontrada</h2>
+            <Link to={'/'} className="text-blue-500 hover:underline">Volver al Inicio</Link>
+          </div>
+        </section>
+      }
+      /></Routes>
   )
 }
 export default RoutesWithNotFound
